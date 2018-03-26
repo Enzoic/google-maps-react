6729e233c20663cc20867969c4e40034b31a8a58
mike <mike@passwordping.com>
2018-03-26 14:49:11 -0600

    Add animation capability to Marker

--- src/__tests__/components/Marker.spec.js
+++ src/__tests__/components/Marker.spec.js
@@ -11,54 +11,58 @@ import Marker from '../../components/Marker';
 // google.maps.LatLng = function(lat, lng, opt_noWrap) {};
 
 describe('Marker', () => {
-  let map = null, google = global.google;
-  let sandbox;
-  let LatLng = null;
-  let location;
+    let map = null, google = global.google;
+    let sandbox;
+    let LatLng = null;
+    let location;
+    let animation;
 
-  beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-
-    map = {}
-    location = {lat: 37.759703, lng: -122.428093}
-
-    sandbox.stub(google.maps, 'Map').returns(google.maps.Map);
-    // sandbox.stub(google.maps, 'Marker').returns(google.maps.Marker);
-  })
-
-  afterEach(() => {
-    sandbox.restore();
-  })
-
-  it('accepts a `map` and a `google` prop', () => {
-    const wrapper = mount(<Marker google={google}
-                                  map={map}
-                                  position={location} />);
-    expect(wrapper.props().google).to.equal(google);
-    expect(wrapper.props().map).to.equal(map);
-  });
-
-  describe('LatLng', () => {
-    let wrapper;
     beforeEach(() => {
-      sandbox.stub(google.maps, 'LatLng')
-        .returns(sinon.createStubInstance(google.maps.LatLng));
-      sandbox.spy(google.maps, 'Marker')
-      wrapper = mount(<Marker google={google}
-                              position={location} />);
+        sandbox = sinon.sandbox.create();
+
+        map = {};
+        location = {lat: 37.759703, lng: -122.428093};
+        animation = 1;
+
+        sandbox.stub(google.maps, 'Map').returns(google.maps.Map);
+        // sandbox.stub(google.maps, 'Marker').returns(google.maps.Marker);
     });
 
-    it('creates a location from the position prop', () => {
-      wrapper.setProps({map: map})
-      sinon.assert
-        .calledWith(google.maps.LatLng, location.lat, location.lng)
+    afterEach(() => {
+        sandbox.restore();
     });
 
-    it('creates a Marker from the position prop', () => {
-      wrapper.setProps({map: map})
-      sinon.assert.called(google.maps.Marker)
+    it('accepts a `map` and a `google` prop', () => {
+        const wrapper = mount(<Marker google={google}
+                                      map={map}
+                                      position={location}
+                                      animation={animation}/>);
+        expect(wrapper.props().google).to.equal(google);
+        expect(wrapper.props().map).to.equal(map);
+        expect(wrapper.props().animation).to.equal(animation);
     });
 
-  })
+    describe('LatLng', () => {
+        let wrapper;
+        beforeEach(() => {
+            sandbox.stub(google.maps, 'LatLng')
+                .returns(sinon.createStubInstance(google.maps.LatLng));
+            sandbox.spy(google.maps, 'Marker')
+            wrapper = mount(<Marker google={google}
+                                    position={location}/>);
+        });
+
+        it('creates a location from the position prop', () => {
+            wrapper.setProps({map: map})
+            sinon.assert
+                .calledWith(google.maps.LatLng, location.lat, location.lng)
+        });
+
+        it('creates a Marker from the position prop', () => {
+            wrapper.setProps({map: map})
+            sinon.assert.called(google.maps.Marker)
+        });
+
+    })
 
 })
--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { camelize } from '../lib/String'
+import {camelize} from '../lib/String'
 
 const evtNames = [
-  'click',
-  'dblclick',
-  'dragend',
-  'mousedown',
-  'mouseout',
-  'mouseover',
-  'mouseup',
-  'recenter',
+    'click',
+    'dblclick',
+    'dragend',
+    'mousedown',
+    'mouseout',
+    'mouseover',
+    'mouseup',
+    'recenter',
 ];
 
-const wrappedPromise = function() {
-    var wrappedPromise = {},
+const wrappedPromise = function () {
+    let wrappedPromise = {},
         promise = new Promise(function (resolve, reject) {
             wrappedPromise.resolve = resolve;
             wrappedPromise.reject = reject;
@@ -29,85 +29,89 @@ const wrappedPromise = function() {
 
 export class Marker extends React.Component {
 
-  componentDidMount() {
-    this.markerPromise = wrappedPromise();
-    this.renderMarker();
-  }
+    componentDidMount() {
+        this.markerPromise = wrappedPromise();
+        this.renderMarker();
+    }
 
-  componentDidUpdate(prevProps) {
-    if ((this.props.map !== prevProps.map) ||
-      (this.props.position !== prevProps.position) ||
-      (this.props.icon !== prevProps.icon)) {
+    componentDidUpdate(prevProps) {
+        if ((this.props.map !== prevProps.map) ||
+            (this.props.position !== prevProps.position) ||
+            (this.props.icon !== prevProps.icon) ||
+            (this.props.animation !== prevProps.animation)) {
+            if (this.marker) {
+                this.marker.setMap(null);
+            }
+            this.renderMarker();
+        }
+    }
+
+    componentWillUnmount() {
         if (this.marker) {
             this.marker.setMap(null);
         }
-        this.renderMarker();
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.marker) {
-      this.marker.setMap(null);
-    }
-  }
-
-  renderMarker() {
-    let {
-      map, google, position, mapCenter, icon, label, draggable, title
-    } = this.props;
-    if (!google) {
-      return null
     }
 
-    let pos = position || mapCenter;
-    if (!(pos instanceof google.maps.LatLng)) {
-      position = new google.maps.LatLng(pos.lat, pos.lng);
+    renderMarker() {
+        let {
+            map, google, position, mapCenter, icon, label, draggable, title, animation
+        } = this.props;
+        if (!google) {
+            return null
+        }
+
+        let pos = position || mapCenter;
+        if (!(pos instanceof google.maps.LatLng)) {
+            position = new google.maps.LatLng(pos.lat, pos.lng);
+        }
+
+        const pref = {
+            map: map,
+            position: position,
+            icon: icon,
+            label: label,
+            title: title,
+            draggable: draggable,
+            animation: animation
+        };
+        this.marker = new google.maps.Marker(pref);
+
+        evtNames.forEach(e => {
+            this.marker.addListener(e, this.handleEvent(e));
+        });
+
+        this.markerPromise.resolve(this.marker);
     }
 
-    const pref = {
-      map: map,
-      position: position,
-      icon: icon,
-      label: label,
-      title: title,
-      draggable: draggable
-    };
-    this.marker = new google.maps.Marker(pref);
-
-    evtNames.forEach(e => {
-      this.marker.addListener(e, this.handleEvent(e));
-    });
-
-    this.markerPromise.resolve(this.marker);
-  }
-
-  getMarker() {
-    return this.markerPromise;
-  }
-
-  handleEvent(evt) {
-    return (e) => {
-      const evtName = `on${camelize(evt)}`
-      if (this.props[evtName]) {
-        this.props[evtName](this.props, this.marker, e);
-      }
+    getMarker() {
+        return this.markerPromise;
     }
-  }
 
-  render() {
-    return null;
-  }
+    handleEvent(evt) {
+        return (e) => {
+            const evtName = `on${camelize(evt)}`
+            if (this.props[evtName]) {
+                this.props[evtName](this.props, this.marker, e);
+            }
+        }
+    }
+
+    render() {
+        return null;
+    }
 }
 
 Marker.propTypes = {
-  position: PropTypes.object,
-  map: PropTypes.object
-}
+    position: PropTypes.object,
+    map: PropTypes.object,
+    animation: PropTypes.number
+};
 
-evtNames.forEach(e => Marker.propTypes[e] = PropTypes.func)
+evtNames.forEach(e => Marker.propTypes[e] = PropTypes.func);
 
 Marker.defaultProps = {
-  name: 'Marker'
-}
+    name: 'Marker',
+    animation: 0
+};
 
 export default Marker
